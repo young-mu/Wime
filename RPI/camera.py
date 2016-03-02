cee62da51fc9360040ffb0ae30f4f0557451d551
young <young.mwy@gmail.com>
2016-03-02 22:31:32 +0800

    delay 1000ms for camera.py

--- RPI/camera.py
+++ RPI/camera.py
@@ -5,7 +5,7 @@ import os
 FILEPATH = "/home/pi/image.jpg"
 
 def getCapture(filepath):
-    os.system("raspistill -w 1024 -h 768 -e jpg -t 1 -o " + filepath)
+    os.system("raspistill -w 1024 -h 768 -e jpg -t 1000 -o " + filepath)
 
 if __name__ == '__main__':
     getCapture(FILEPATH)
